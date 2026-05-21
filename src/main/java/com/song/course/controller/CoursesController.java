3a79fe0be102dd1651d40949941913c0601be9dd
daniel2825 <daniel2825@hotmail.com>
2026-05-21 18:58:39 -0500

    Subscribe person back end

--- src/main/java/com/song/course/controller/CoursesController.java
+++ src/main/java/com/song/course/controller/CoursesController.java
@@ -1,9 +1,19 @@
 package com.song.course.controller;
+import com.song.course.model.Person;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 
 @Controller
 public class CoursesController {
 
+    @MutationMapping
+    @Transactional
+    public void subscribePersonToCourse(
+            @Argument Person person) {
+        System.out.println("hello Suscribe course"+person.getEmail());
+    }
 
 
 
