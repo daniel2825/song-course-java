822585a1b484d0e63ccb5006b6a91f3e3bb2c605
daniel2825 <daniel2825@hotmail.com>
2026-07-15 09:21:22 -0500

    Update kafka services.

--- src/main/java/com/song/course/services/CoursesServices.java
+++ src/main/java/com/song/course/services/CoursesServices.java
@@ -4,6 +4,7 @@ import com.song.course.model.Courses;
 import com.song.course.model.Person;
 import com.song.course.repository.ICoursesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +16,11 @@ public class CoursesServices {
         this.coursesRepository = coursesRepository;
     }
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     public void subscribeCourse(final Person person, final Courses course) {
+        kafkaTemplate.send("subscribe-course", "key", "Send course topic kafka");
         coursesRepository.subscribeCourse(person.getEmail(), course.getIdCourse(), course.getTitle());
         coursesRepository.relationsPersonAndCourse(person.getEmail(), course.getIdCourse(), course.getTitle());
     }
